c45daf2e1c90e9cccf7787b9c2d68bee460555c3
TK <tk@xarby.com>
2020-08-23 16:05:19 -0700

    j

--- software/spronkler.py
+++ software/spronkler.py
@@ -307,7 +307,7 @@ class Spronkler():
                         
                         # actually check
                         if (j_newstart >= i_start and j_newstart < i_end) or (j_newend > i_start and j_newend <= i_end):
-                            print("{{0} >= {2} and {0} < {3}) or ({1} > {2} and {1} <= {3})".format(j_newstart, j_newend, i_start, i_end))
+                            print("({0} >= {2} and {0} < {3}) or ({1} > {2} and {1} <= {3})".format(j_newstart, j_newend, i_start, i_end))
                             conflict_detected = True
                             conflicting_schedule = schedule
                             
