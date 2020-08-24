f715c8a74132e6d0b179538ed5304b15d237df6c
Thomas Kinsey <tk@xarby.com>
2020-08-24 16:20:00 -0700

    date format shenanigans

--- software/spronkler.py
+++ software/spronkler.py
@@ -401,6 +401,7 @@ class Spronkler():
                         start_time = datetime.datetime.strptime(self.schedules[i]['start_time'], self.dateformat)
                         end_time = datetime.datetime.strptime(self.schedules[i]['end_time'], self.dateformat)
                         now_raw = datetime.datetime.now()
+                        self.log("{} -> {}".format(now_raw.isoformat(), "{}-".format(now_raw.year) + self.dateformat))
                         now = datetime.datetime.strptime(now_raw.isoformat(), "{}-".format(now_raw.year) + self.dateformat)
                         now.replace(year=1900)
                         self.log("Should I start Schedule '{}'?  It starts at {} and ends at {}.  Currently, it's {}.".format(self.schedules[i]['name'], start_time, end_time, now))
