6e75694a326d1e17bd06080a0d81a11de87a85c9
Thomas Kinsey <tk@xarby.com>
2020-08-26 08:38:07 -0700

    8:39

--- software/spronkler.py
+++ software/spronkler.py
@@ -414,7 +414,7 @@ class Spronkler():
                         if start_time <= now and end_time > now and self.schedules[i]['running'] == False:
                         
                             # then make sure we're close the the start hour/minute
-                            self.log("Timedelta: {}:{}".format(now.hour - start_time.hour, abs(now.minute - start_time.minute)))
+                            self.log("{} timedelta: {}:{}".format(schedule['name'], now.hour - start_time.hour, abs(now.minute - start_time.minute)))
                             if now.hour == start_time.hour and abs(now.minute - start_time.minute) < 5:
                                 self.__runSchedule(self.schedules[i])
                                 self.schedules[i]['running'] = True
