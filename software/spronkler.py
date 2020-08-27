4d4ff08e93d13d6e8e5f58eccdfdbed5a88e8606
Thomas Kinsey <tk@xarby.com>
2020-08-26 17:30:56 -0700

    d

--- software/spronkler.py
+++ software/spronkler.py
@@ -376,7 +376,8 @@ class Spronkler():
                             #advance next run to the first time this schedule runs *after* now!
                             while msg.schedule['nextrun'] < time.time():
                                 addtime = msg.schedule['interval_minutes'] * 60
-                                self.log("{}, {}".format(addtime, datetime.datetime.fromtimestamp(msg.schedule['nextrun'])))
+                                if msg.schedule['name'] == "Eight Thirty":
+                                    self.log("{}, {}".format(addtime, datetime.datetime.fromtimestamp(msg.schedule['nextrun'])))
                                 msg.schedule['nextrun'] += addtime
                      
                             self.schedules.append(msg.schedule)
